a5eabd2b70204d6528bdf123dd3567843f4397d4
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2025-12-30 18:14:09 +0100

    update puthon_module_03/ex0/ft_command_quest.py

--- python_module_03/ex0/ft_command_quest.py
+++ python_module_03/ex0/ft_command_quest.py
@@ -3,14 +3,14 @@ import sys
 if __name__ == "__main__":
     length = len(sys.argv)
     print("=== Command Quest ===")
-    if len(sys.argv) == 1:
+    if length == 1:
         print("No arguments provided!")
         print("Program name:", sys.argv[0])
     else:
         print("Program name:", sys.argv[0])
         print("Arguments received:", length - 1)
         i = 1
-        while i < length:
-            print(f"Argument {i}: {sys.argv[i]}")
+        for arg in sys.argv[1:]:
+            print(f"Argument {i}: {arg}")
             i += 1
     print("Total arguments:", length)
