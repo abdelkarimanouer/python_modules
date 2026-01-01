cd4d35a1f6201a1f3151a8e878680c168955f5e8
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-01-01 10:40:59 +0100

    update puthon_module_03/ex1/ft_score_analytics.py

--- python_module_03/ex1/ft_score_analytics.py
+++ python_module_03/ex1/ft_score_analytics.py
@@ -4,19 +4,21 @@ if __name__ == "__main__":
     try:
         print("=== Player Score Analytics ===")
         if len(sys.argv) > 1:
-            mylist = []
+            mylist: list[int] = []
             for e in sys.argv[1:]:
-                mylist.append(int(e))
+                score = int(e)
+                mylist.append(score)
             total_number = len(mylist)
             sum_numbers = sum(mylist)
             max_number = max(mylist)
             min_number = min(mylist)
             print("Scores processed:", mylist)
             print("Total players:", total_number)
+            print("Total score:", sum_numbers)
             print("Average score:", sum_numbers / total_number)
             print("High score:", max_number)
             print("Low score:", min_number)
-            print("Score range:", )
+            print("Score range:", max_number - min_number)
         else:
             print(f"No scores provided. Usage: python3 {sys.argv[0]} "
                   f"<score1> <score2> ...")
